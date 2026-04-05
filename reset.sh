443c900f6fa28d31bad677287f9d7ca09f8376e7
xyleneSVG <dhavinf.a@gmail.com>
2026-04-06 01:55:41 +0700

    fix: readjust flow setup

--- reset.sh
+++ reset.sh
@@ -3,5 +3,7 @@
 echo "🔥 Hapus semua container UKK..."
 
 docker rm -f $(docker ps -a --filter "name=ukk_" -q) 2>/dev/null
+docker rm -f $(docker ps -a --filter "name=proxysql_ukk" -q) 2>/dev/null
+docker rm -f $(docker ps -a --filter "name=phpmyadmin_ukk" -q) 2>/dev/null
 
 echo "✅ Bersih!"
